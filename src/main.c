91ce4b184b966d2b2fd786398623923213ea07b4
lz1irq <ivo@ludost.net>
2016-05-07 23:17:08 +0300

    Add makefile

--- src/main.c
+++ src/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include "mstg_mp3.h"
 
-int main() {
-    mstg_mp3_t *mp3 = mstg_mp3_new("./geneva.mp3");
-    printf("ID3v2 header size: %d\n", mstg_mp3_get_tag_size(mp3));
+int main(int argc, char** argv) {
+    if (argc >= 2) {
+        mstg_mp3_t *mp3 = mstg_mp3_new(argv[1]);
+        printf("ID3v2 header size: %d\n", mstg_mp3_get_tag_size(mp3));
+    }
 
 }
