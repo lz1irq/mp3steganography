82434efe19989d6e1acb93827003c48d12de231c
lz1irq <ivo@ludost.net>
2016-05-24 00:01:43 +0300

    Add writing hidden messages to MP3

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "mstg_mp3.h"
 
 int main(int argc, char** argv) {
@@ -6,6 +7,11 @@ int main(int argc, char** argv) {
         mstg_mp3_t *mp3 = mstg_mp3_new(argv[1]);
         printf("ID3v2 header size: %d\n", mstg_mp3_get_tag_size(mp3));
         printf("Frame size: %d\n", mstg_mp3_frame_size(mp3));
+        
+        char* msg = "secret message here";
+        mstg_mp3_write_message(mp3, msg, strlen(msg), 0x321fa);
+
+        printf("WRITE: %d\n", mstg_mp3_writeout(mp3));
     }
 
 }
--- src/mstg_util.h
+++ src/mstg_util.h
@@ -1,3 +1,4 @@
 # include <sys/types.h>
 off_t get_file_size(const char* file_path);
 char* read_file(FILE* fp, off_t fsize);
+size_t write_file(FILE* fp, char* content, size_t content_len);
